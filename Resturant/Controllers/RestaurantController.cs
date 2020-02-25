fbd0e292c45993f1649128d5e7677865bab1720a
Jess Schultz <schultz.jess.a@outlook.com>
2020-02-25 13:45:23 -0600

    Update

--- Resturant/Controllers/RestaurantController.cs
+++ Resturant/Controllers/RestaurantController.cs
@@ -1,6 +1,7 @@
 ﻿using Resturant.Models;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
@@ -12,6 +13,8 @@ namespace RestaurantRater.Controllers
     public class RestaurantController : ApiController
     {
         private readonly RestaurantDbContext _context = new RestaurantDbContext();
+
+        [HttpPost]
         public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
         {
             if(ModelState.IsValid && restaurant != null)
@@ -22,5 +25,52 @@ namespace RestaurantRater.Controllers
             }
             return BadRequest(ModelState);
         }
+
+        //Get All
+        [HttpGet]
+        public async Task<IHttpActionResult> GetAll()
+        {
+            List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+
+            return Ok(restaurants);
+        }
+
+        //Get By Id
+        [HttpGet]
+        public async Task<IHttpActionResult> GetById(int Id)
+        {
+            Restaurant restaurant = await _context.Restaurants.FindAsync(Id);
+            if( restaurant == null)
+            {
+                return NotFound();
+            }
+            return Ok(restaurant);
+        }
+
+        //Put(Update)
+        [HttpPut]
+        public async Task<IHttpActionResult> UpdateRestaurant([FromUri]int id, [FromBody]Restaurant model)
+        {
+            if (ModelState.IsValid && model != null)
+            {
+                Restaurant restaurant = await _context.Restaurants.FindAsync(id);
+
+                if(restaurant != null)
+                {
+                    restaurant.Name = model.Name;
+                    restaurant.Rating = model.Rating;
+                    restaurant.Style = model.Style;
+                    restaurant.DollarSigns = model.DollarSigns;
+
+                    await _context.SaveChangesAsync();
+                    return Ok();
+                }
+                return NotFound();
+            }
+            return BadRequest(ModelState);
+
+        }
+
+        //Delete
     }
 }
---
+++ Resturant/Migrations/202002251901251_changed_properties.cs
@@ -0,0 +1,18 @@
+﻿namespace RestaurantRater.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class changed_properties : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.Restaurants", "Symbol", c => c.String());
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.Restaurants", "Symbol");
+        }
+    }
+}
