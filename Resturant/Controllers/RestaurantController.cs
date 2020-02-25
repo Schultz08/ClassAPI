a0e6d01ac0e530e47f85187b0a2d9cca3c95ec02
Jess Schultz <schultz.jess.a@outlook.com>
2020-02-25 12:39:36 -0600

    Initial

---
+++ Resturant/Controllers/RestaurantController.cs
@@ -0,0 +1,26 @@
+﻿using Resturant.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace RestaurantRater.Controllers
+{
+    public class RestaurantController : ApiController
+    {
+        private readonly RestaurantDbContext _context = new RestaurantDbContext();
+        public async Task<IHttpActionResult> PostRestaurant(Restaurant restaurant)
+        {
+            if(ModelState.IsValid && restaurant != null)
+            {
+            _context.Restaurants.Add(restaurant);
+            await _context.SaveChangesAsync();
+            return Ok();
+            }
+            return BadRequest(ModelState);
+        }
+    }
+}
--- Resturant/Models/Restaurant.cs
+++ Resturant/Models/Restaurant.cs
@@ -8,6 +8,14 @@ namespace Resturant.Models
 {
     public class Restaurant
     {
+     /*        public Restaurant() { }
+        public Restaurant(int dollarSigns)
+        {
+            DollarSigns = dollarSigns;
+
+        }*/
+
+
         [Key]
         public int Id { get; set; }
 
@@ -24,6 +32,31 @@ namespace Resturant.Models
         [Required]
         [Range(1, 5)]
         public int DollarSigns { get; set; }
-        public char Symbol { get; }
+        public string Symbol 
+        { 
+            get 
+            {
+            switch (DollarSigns)
+            {
+                case 1:
+                    Symbol = "$";
+                        break;
+                case 2:
+                    Symbol = "$$";
+                        break;
+                case 3:
+                    Symbol = "$$$";
+                        break;
+                case 4:
+                    Symbol = "$$$$";
+                        break;
+                case 5:
+                   Symbol = "$$$$$";
+                        break;
+            }
+                return null;
+            }
+            set { }
+        }
     }
 }
